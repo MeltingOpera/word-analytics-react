9e696e25a949f68e4b8bae1d7b372942cd3041b3
Patrick Weis <85198240+MeltingOpera@users.noreply.github.com>
2024-11-24 20:17:45 +0000

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="footer">
-      <small>&copy; Copyright by ByteGrad. All rights reserved.</small>
+      <small>Copyright &copy; 2024</small>
       <small>Version: 1.0.0</small>
     </footer>
   );
